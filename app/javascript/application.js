feb0f6f61f17786c1e4a52c6a983b6ab567b20dc
zanozano <zanozanozano@gmail.com>
2023-06-14 11:17:47 +0700

    font awesome

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require turbolinks
+//= require fontawesome/all
+//= require_tree .
 import "@hotwired/turbo-rails"
 import "controllers"
+
+FontAwesome.config.mutateApproach = 'sync'
